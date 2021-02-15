a02410224599ee5cc9b5a4cd10382e0229f83022
GolamRabby96 <golamrabbynwu@gmail.com>
2021-02-16 00:05:19 +0600

    undefine

---
+++ undefine-vs-null.js
@@ -0,0 +1,25 @@
+/*let pakhi;
+console.log(pakhi);
+*/
+
+/*
+function add (a,b){
+    console.log(a,b); // b parameter missing so undefine
+}
+
+const result = add(5);
+*/
+/*
+const pre = {name: 'smart', phone:'015156451254'}
+console.log(pre.gf); // property nai so undefine 
+*/
+/*
+let fun = undefined; // by default undefined
+console.log(fun);
+*/
+/*
+let age = [2,5,8]
+consol.log(age[11]) // undefine because of age[11] is no more in this array
+*/
+
+// ...... null ... .. .. ... ... ... /
