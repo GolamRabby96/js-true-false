73669c5f658a4887f2d42382e6a2a09a52123da6
GolamRabby96 <golamrabbynwu@gmail.com>
2021-02-15 23:53:02 +0600

    true vs false

---
+++ true-false.js
@@ -0,0 +1,26 @@
+// false value 
+// 0
+// ""
+// undefine
+//null
+//NaN
+// ai gule if  else ar vitor a false dibe
+
+
+const age = -4;
+
+if(age){ console.log("condition is true");}
+else{console.log("condition is false");}
+
+
+// if(age) ai vabe dile age 0 bad a sod kisu true hobe
+
+const name = 'golam Rabby'
+if(name){console.log("condition is true");}
+else{console.log("condition is false");}
+
+//if(name) amon hole '' balank string bad a sob kisui true 
+
+let nam =null;
+if(nam){console.log("condition is true");}
+else{console.log("condition is false");}
